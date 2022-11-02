5d35594982752b8621ad6c4af25227b58e159298
Anton Rak <Anton_Rak@epam.com>
2022-11-03 00:21:55 +0300

    publish HTML report

--- .mocharc.js
+++ .mocharc.js
@@ -1,7 +1,7 @@
 module.exports = {
     spec: "./test.spec.js",
     //require: "chai/register-expect.js",
-    reporter: "mocha-junit-reporter",
+    reporter: "mocha-simple-html-reporter",
     reporterOptions: {
         mochaFile: "src/report/reports.xml"
     }
