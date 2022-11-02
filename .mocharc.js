a3a8b3f67e1e508b55142e8fc4f5141f4bcfe1d6
Anton Rak <Anton_Rak@epam.com>
2022-11-02 22:24:38 +0300

    report junit

--- .mocharc.js
+++ .mocharc.js
@@ -1,7 +1,7 @@
 module.exports = {
     spec: "./test.spec.js",
     //require: "chai/register-expect.js",
-    reporter: ["mocha-junit-reporter", "spec"],
+    reporter: ["mocha-junit-reporter"],
     reporterOptions: {
         mochaFile: "./report/reports.xml"
     }
