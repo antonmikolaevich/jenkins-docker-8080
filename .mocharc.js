687db3eec8af0925763481e49e85c54dfdb83579
Anton Rak <Anton_Rak@epam.com>
2022-11-04 18:09:34 +0300

    jenkinsDockerReport

--- .mocharc.js
+++ .mocharc.js
@@ -1,8 +1,8 @@
 module.exports = {
     spec: "./test.spec.js",
-    require: "chai/register-expect.js",
+   // require: "chai/register-expect.js",
     reporter: "mocha-junit-reporter",
-    reporterOptions: {
-        mochaFile: "./report/reports.xml"
-    }
+    //reporterOptions: {
+    //    mochaFile: "./reporter01/reports.xml"
+    //}
 }
