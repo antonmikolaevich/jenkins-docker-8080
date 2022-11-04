cd7e1878034db9e2fa08cb4035053c0e24f08656
Anton Rak <Anton_Rak@epam.com>
2022-11-04 20:51:14 +0300

    add twose

--- .mocharc.js
+++ .mocharc.js
@@ -1,8 +1,8 @@
 module.exports = {
     spec: "./test.spec.js",
-   // require: "chai/register-expect.js",
-    reporter: "mocha-junit-reporter",
-    //reporterOptions: {
-    //    mochaFile: "./reporter01/reports.xml"
-    //}
+   require: "chai/register-expect.js",
+    reporter: ["mocha-junit-reporter"],
+    reporterOptions: {
+       mochaFile: "./reporter01/reports.xml"
+    }
 }
