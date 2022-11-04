286786baedfff661ccd734af2136bf50a5eac215
Anton Rak <Anton_Rak@epam.com>
2022-11-04 20:55:10 +0300

    one left reg

--- .mocharc.js
+++ .mocharc.js
@@ -1,6 +1,5 @@
 module.exports = {
     spec: "./test.spec.js",
-    require: "chai/register-expect.js",
     reporter: ["mocha-junit-reporter"],
     reporterOptions: {
        mochaFile: "./reporter01/reports.xml"
